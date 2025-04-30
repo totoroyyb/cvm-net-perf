9507bbc74029522b2a4d6f6f8b6e64543de54fe6
Yibo Yan <yiboyan@usc.edu>
2025-04-30 01:50:18 +0000

    feat: include profiler build in the script

--- hires-logger/build.sh
+++ hires-logger/build.sh
@@ -3,10 +3,14 @@
 mkdir -p build
 
 pushd build
-cmake .. -DCMAKE_BUILD_TYPE=Debug
+cmake .. -DCMAKE_BUILD_TYPE=Release
 make -j$(nproc)
 popd
 
 pushd khires
 make -j$(nproc)
+popd
+
+pushd profiler
+cargo build --release
 popd
